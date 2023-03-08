f2e9535b2f0b31c12fd6824bb582b9315ca82ab9
julia <julia.albq@gmail.com>
2023-03-08 16:56:48 -0300

    refactor: Implement i18n for notifications

--- src/components/Notification.jsx
+++ src/components/Notification.jsx
@@ -1,21 +1,31 @@
 import cx from "./Notification.module.scss";
-import { useNotification } from "../hooks/useNotification"
+import { useNotification } from "../hooks/useNotification";
 import { useEffect } from "react";
+import { useTranslation } from "../hooks/useTranslation";
 
 export const Notification = () => {
-  const {notification, changeNotification} = useNotification()
+  const { notification, changeNotification } = useNotification();
+  const { t } = useTranslation();
 
   useEffect(() => {
-    if(notification !== "") {
+    notification === "Entry added" && changeNotification(t("entryAdded"));
+
+    notification === "Entry edited" && changeNotification(t("entryEdited"));
+
+    notification === "Entry deleted" && changeNotification(t("entryDeleted"));
+  }, [notification]);
+
+  useEffect(() => {
+    if (notification !== "") {
       setTimeout(() => {
-        changeNotification("")
+        changeNotification("");
       }, 2500);
     }
-  }, [notification])
+  }, [notification]);
 
   return (
     <div className={`${cx.container} ${notification === "" ? cx.empty : ""}`}>
       <p>{notification}</p>
     </div>
-  )
-}
+  );
+};
